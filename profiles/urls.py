a0d6ef42238826011d44da35995155076b797d33
Beyzanur <beyzanurdincer502@gmail.com>
2025-07-23 15:33:13 +0300

    finally commit

--- profiles/urls.py
+++ profiles/urls.py
@@ -2,7 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('profile/<str:username>/', views.profile_detail, name='profile_detail'),  # Öğrenci profili
-    path('profile/<str:username>/edit/', views.profile_edit, name='profile_edit'),  # Profil düzenleme (opsiyonel)
-    path('company/<slug:slug>/', views.company_profile, name='company_profile'),  # Şirket profili
+    path('profile/redirect/', views.profile_redirect, name='profile_redirect'),
+    path('profile/<str:username>/edit/', views.profile_edit, name='profile_edit'),
+    path('profile/<str:username>/', views.profile_detail, name='profile_detail'),
+    path('company/<slug:slug>/', views.company_profile, name='company_profile'),
 ]
